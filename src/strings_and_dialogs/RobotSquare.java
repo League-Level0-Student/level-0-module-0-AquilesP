a0f43282b68b389a3e35e87b9c394cd10f2e4cf9
codeclub <codeclub@smms-rm23-89.smusd.local>
2019-01-23 14:38:24 -0800

    Robot

--- src/strings_and_dialogs/RobotSquare.java
+++ src/strings_and_dialogs/RobotSquare.java
@@ -12,22 +12,24 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot Pewdiepie=new Robot();
 
         // 3. Put the robot's pen down
-
+Pewdiepie.penDown();
 
         // 6. Make the robot move as fast as possible
-
+Pewdiepie.setSpeed(4000);
 
         // 5. Do everything below here 4 times
+for (int i = 0; i <40000; i++) {
+ 
 
 
         //         2. Move your robot 200 pixels
-
+Pewdiepie.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
+Pewdiepie.turn( 111);
+}
     }
 }
